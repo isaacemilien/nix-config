f4aaab16dbd81ab030d315e28134d9c7e72b1069
Isaac Emilien <isaac.emilien@gmail.com>
2026-09-01 20:57:33 +0100

    chore: update to unstable for latest yt-dlp

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "My flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     playlist-select.url = "github:isaacemilien/playlist-select";
     tiny-terminal-timer.url = "github:isaacemilien/tiny-terminal-timer";
